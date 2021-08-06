08828846133b88016a6f89cf232f49b61f48be1a
pokiuwu <72038114+pokiuwu@users.noreply.github.com>
2021-08-06 22:54:09 +0300

    Update MapleHook.cpp

--- Client176/MapleHook.cpp
+++ Client176/MapleHook.cpp
@@ -34,7 +34,7 @@ void InitStringPool()
 {
 	g_mStringPool[4479] = std::string("Poki v176"); //Login Version
 	g_mStringPool[29] = std::string("Ayame"); // Manager Job to Ayame
-	g_mStringPool[1547] = std::string("[Welcome] Welcome to Poki v176 !!"); //version
+	g_mStringPool[1547] = std::string("[Welcome] Welcome to Poki v176 !!"); //Welcome message
 }
 
 void FuckMaple()
